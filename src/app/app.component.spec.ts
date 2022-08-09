949a6824f080e28830c8508c33c084804fbf84eb
yasincayir <cayir.yasinn@gmail.com>
2022-08-09 22:24:46 +0300

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,7 +8,7 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule
       ],
-      declarations: [
+      declarations: [ 
         AppComponent
       ],
     }).compileComponents();
